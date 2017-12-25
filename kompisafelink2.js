3bc62580418c7a18536251076bb602371bba5693
nurhasan25 <34822079+nurhasan25@users.noreply.github.com>
2017-12-26 05:54:29 +0700

    Update kompisafelink2.js

--- kompisafelink2.js
+++ kompisafelink2.js
@@ -25,4 +25,4 @@ if(a_to_vk)
 {a_to_vk.innerHTML+=a_to_va;}}
 function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
-
+protected_links = "oload.stream,tusfiles.net,userscloud.com,linkedin.com,twitter.com,youtube.com,facebook.com";
